612d2f611ff67dfca1b13a47f7f940e58f8df0b6
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-13 14:13:47 -0700

    add #4 answer

--- ES10-Exercise.js
+++ ES10-Exercise.js
@@ -17,6 +17,7 @@ greeting.flatMap(word => word.join(' ')).join(' ')
 //#4 Turn the trapped 3 number into: [3]
 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
 
+trapped.flat(25)
 
 //#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
 const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
