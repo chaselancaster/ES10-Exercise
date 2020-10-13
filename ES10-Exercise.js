1c495774b3962d1ef858da080b5d920664fa974a
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-13 14:27:16 -0700

    add #8 answer

--- ES10-Exercise.js
+++ ES10-Exercise.js
@@ -36,3 +36,4 @@ const updatedArray = usersArray.map(user => [user[0], user[1] * 2])
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
 
+const newObject = Object.fromEntries(updatedArray);
