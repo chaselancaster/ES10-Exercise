ebed43a764f0a1ebdd8806c7a2f465ffcb901a5a
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-13 14:16:53 -0700

    add #6 answer

--- ES10-Exercise.js
+++ ES10-Exercise.js
@@ -27,6 +27,7 @@ userEmail3.trimStart().trimEnd()
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 }
 
+Object.entries(users);
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
 
