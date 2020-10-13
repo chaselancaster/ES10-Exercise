27f00644706507a14e47557c2f981f03e3163618
Chase Lancaster <chaseblancaster@gmail.com>
2020-10-13 14:12:36 -0700

    Add #3 answer

--- ES10-Exercise.js
+++ ES10-Exercise.js
@@ -12,11 +12,10 @@ greeting.flatMap(word => word.join(' '))
 
 //#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
 
-
+greeting.flatMap(word => word.join(' ')).join(' ')
 
 //#4 Turn the trapped 3 number into: [3]
 const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
-
 
 
 //#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
